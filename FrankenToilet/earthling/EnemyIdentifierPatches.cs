6aa6b1fd59b8886649707d2a10c7344e9c42e630
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2026-01-30 19:18:20 +0000

    feat: make killing an enemy result in catching a random fish

---
+++ FrankenToilet/earthling/EnemyIdentifierPatches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using HarmonyLib;
+using FrankenToilet.Core;
+
+namespace FrankenToilet.earthling;
+
+[PatchOnEntry]
+[HarmonyPatch(typeof(EnemyIdentifier))]
+public class EnemyIdentifierPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("Death", new Type[]{typeof(bool)})]
+    public static void SpawnFishOnDeath(EnemyIdentifier __instance) 
+    {
+        if (__instance.dead) return;
+
+        FishObject fish = FishProvider.GetRandomFish();
+        ItemIdentifier fishPickup = FishProvider.CreateFishPickup(fish);
+        GameObject fishInstance = GameObject.Instantiate(fishPickup.gameObject, __instance.transform.position, __instance.transform.rotation, __instance.transform.parent);
+        fishInstance.GetComponent<Rigidbody>().velocity += Vector3.up * 50;
+
+        FishingHUD.Instance.ShowHUD();
+        FishingHUD.Instance.ShowFishCaught(true, fish);
+        FishManager.Instance.UnlockFish(fish);
+    }
+}
